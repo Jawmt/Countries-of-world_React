ce4a90b605fa1ba7c39e49a795e160a2fc080d15
Jawad MBT <jawadmbt@gmail.com>
2022-09-10 18:48:32 +0200

    Routing implementation

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import { useEffect, useState } from 'react'
 import { countriesServices } from './services/countriesService';
 import HomeCountries from './pages/HomeCountries';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
    
 
   return (
-    <>
-      <HomeCountries />    
-    </>
+    <Routes>
+      <Route path="/countries" element={<HomeCountries />}/>
+      <Route path="/countries/:id" />
+    </Routes>
   );
 }
 
